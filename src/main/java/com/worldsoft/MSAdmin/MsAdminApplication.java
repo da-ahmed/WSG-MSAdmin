599bb79b43adcd0746337e9ef6d191f2d25c2e80
da-ahmed <bbarkouti@gmail.com>
2018-06-06 14:46:22 +0100

    6/6/18

--- src/main/java/com/worldsoft/MSAdmin/MsAdminApplication.java
+++ src/main/java/com/worldsoft/MSAdmin/MsAdminApplication.java
@@ -1,12 +1,26 @@
 package com.worldsoft.MSAdmin;
 
+import com.worldsoft.MSAdmin.dao.RoleDAO;
+import com.worldsoft.MSAdmin.entities.Role;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class MsAdminApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsAdminApplication.class, args);
+		ApplicationContext context = SpringApplication.run(MsAdminApplication.class, args);
+                
+                RoleDAO roleDao = context.getBean(RoleDAO.class);
+                
+                Role roleUser = new Role();
+            Role roleAdmin = new Role();
+            roleUser.setRoleName("STANDARD_USER");
+            roleUser.setDescription("Standard User - N'a pas de droits admin");
+            roleDao.save(roleUser);
+            roleAdmin.setRoleName("ADMIN_USER");
+            roleAdmin.setDescription("Admin User - Il a les permissions d'effectuer les taches d'un admin");
+            roleDao.save(roleAdmin);
 	}
 }
